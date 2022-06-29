14901c7ce5e815f492dc0d440534858864c35972
ericruga <ericruga@users.noreply.github.com>
2022-06-29 18:11:55 +0200

    Iteration 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -16,6 +16,12 @@ router.get("/signup", (req, res, next) => {
 router.post('/signup', (req, res, next) => {
     //console.log('The form data: ', req.body);
     const { username, password } = req.body;
+
+    // make sure users fill all mandatory fields:
+    if (!username || !password) {
+    res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username and password.' });
+    return;
+  }
     
     bcryptjs
     .genSalt(saltRounds)
@@ -32,9 +38,56 @@ router.post('/signup', (req, res, next) => {
         res.redirect('/userProfile');
 
     })
-    .catch(error => next(error));
+    .catch(error => {
+        // copy the following if-else statement
+    if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render('auth/signup', { errorMessage: error.message });
+    } else {
+        next(error);
+    }
+  });
+})
+
+router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
+router.get('/login', (req, res) => res.render('auth/login'));
+
+router.post('/login', (req, res, next) => {
+    console.log('SESSION =====> ', req.session);
+    const { username, password } = req.body;
+   
+    if (username === '' || password === '') {
+      res.render('auth/login', {
+        errorMessage: 'Please enter both, username and password to login.'
+      });
+      return;
+    }
+   
+    User.findOne({ username })
+      .then(user => {
+        if (!user) {
+          res.render('auth/login', { errorMessage: 'This user is not registered. Try with other one.' });
+          return;
+        } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+          //res.render('users/user-profile', { user });
+          req.session.currentUser = user;
+          res.redirect('/userProfile');
+        } else {
+          res.render('auth/login', { errorMessage: 'Incorrect password.' });
+        }
+      })
+      .catch(error => next(error));
   });
 
-  router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+  router.get('/userProfile', (req, res) => {
+    res.render('users/user-profile', { userInSession: req.session.currentUser });
+  });
+
+  router.post('/logout', (req, res, next) => {
+    req.session.destroy(err => {
+      if (err) next(err);
+      res.redirect('/');
+    });
+  });
  
 module.exports = router;
