f87ca652dcbc9af3a89e50fd6758184a6109ab24
ericruga <ericruga@users.noreply.github.com>
2022-06-29 16:19:24 +0200

    Iteration 1

--- models/User.model.js
+++ models/User.model.js
@@ -4,11 +4,16 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
+    trim: true,
+    required: [true, 'Username is required.'],
     unique: true
   },
-  password: String
+  passwordHash: {
+    type: String,
+    required: [true, 'Password is required.'],
+  },
+
 });
 
 const User = model("User", userSchema);
-
 module.exports = User;
